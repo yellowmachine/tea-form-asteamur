92ea05ede4e3704e22a52900d99526cf160b159d
Miguel A. Alarcos <yellow.machine@mailfence.com>
2022-11-28 16:52:22 +0100

    added query.ts

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
-    import Formgenerator from "@/lib/form/FormGenerator.svelte";
+    //import Formgenerator from "@/lib/form/FormGenerator.svelte";
     import { elements, formPageOptions as options } from "@/lib/user";
 
-    let id = null;
-    //$: id = decodeURIComponent($params.id || null)
+    //let id = null;
 </script>
 
+hello ;)
+<!--
 <main>
   <Formgenerator {id} {options} {elements} />
 </main>
+-->
